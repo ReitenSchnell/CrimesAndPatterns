4f4d3301012fbfed46f717b0031453d3677c623b
Alina <alina542.1@gmail.com>
2016-06-29 20:26:35 +0300

    bad attempt to include d3

--- CEClient/app/directives/barChart.js
+++ CEClient/app/directives/barChart.js
@@ -6,6 +6,9 @@ angular
       scope: {},
       link: function(scope, element, attrs){
         d3Service.d3().then(function(d3){
+          var margin = parseInt(attrs.margin) || 20;
+          var barHeight = parseInt(attrs.barHeight) || 20;
+          var barPadding = parseInt(attrs.barPadding) || 5;
           var svg = d3.select(element[0])
             .append('svg')
             .style('width', '100%');
@@ -24,7 +27,25 @@ angular
             scope.render(scope.data);
           });
           scope.render = function(data){
-            
+            svg.selectAll('*').remove();
+            if(!data)
+              return;
+            var width = d3.select(element[0]).node.offsetWidth - margin;
+            var height = scope.data.length*(barHeight+barPadding);
+            var color = d3.scale.category20();
+            var xScale = d3.scale.linear().domain([0, d3.max(data, function(d) {
+              return d.score;
+            })]).range([0, width]);
+            svg.attr('height', height);
+            svg.selectAll('rect').data(data).enter().append('rect')
+              .attr('height', barHeight).attr('width', 140).attr('x', Math.round(margin/2))
+              .attr('y', function(d,i){
+                return i*(barHeight + barPadding);
+              })
+              .attr('fill', function(d){return color(d.score);})
+              .transition().duration(1000).attr('width', function(d){
+                return xScale(d.score);
+              });
           }
         })
       }
--- CEClient/app/index.config.js
+++ CEClient/app/index.config.js
@@ -2,9 +2,9 @@ angular
   .module('crimeChartApp')
   .config(function ($logProvider) {
     // Enable log
-    $logProvider.debugEnabled(true);
+    //$logProvider.debugEnabled(true);
   })
   .config(function ($httpProvider) {
-    $httpProvider.defaults.withCredentials = true;
+    //$httpProvider.defaults.withCredentials = true;
   })
  ;
