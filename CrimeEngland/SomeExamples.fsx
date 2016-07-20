f19e3d9d7a554e7f0bee1305ef48e7af21e70bd5
Alina <alina542.1@gmail.com>
2016-07-20 19:46:24 +0300

    refines some css and adds technologies description

--- CrimeEngland/SomeExamples.fsx
+++ CrimeEngland/SomeExamples.fsx
@@ -38,3 +38,27 @@ let uppercase = p.UppercaseName
 let i = 20
 let even = i.IsEven
 let p2 = Person.T.Populate "Foo" "Boo"
+
+
+type Stack = StackContents of float list
+
+let newStack = StackContents [1.0; 2.0; 3.0]
+
+let (StackContents contents) = newStack
+
+let push x (StackContents contents) =
+    StackContents(x :: contents)
+
+let emptyStack = StackContents []
+let stackWith1 = push 1.0 emptyStack
+let stackWith2 = push 2.0 stackWith1
+
+let ONE = push 1.0
+let TWO = push 2.0
+let THREE = push 3.0
+let FOUR = push 4.0
+let FIVE = push 5.0
+let EMPTY = StackContents []
+
+let result321 = EMPTY |> ONE |> TWO |> THREE
+let result123 = EMPTY |> THREE |> TWO |> ONE
